e210c2a40fdd62fddcfb497af70e2def8559d400
Rohit Kumar <rohit09619@gmail.com>
2023-02-01 18:53:30 +0530

    Added ResponseEntity

--- src/main/java/com/codingbz/studentManagement/StudentController.java
+++ src/main/java/com/codingbz/studentManagement/StudentController.java
@@ -1,6 +1,8 @@
 package com.codingbz.studentManagement;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -13,23 +15,33 @@ public class StudentController {
 
     // get information
     @GetMapping("/get_student")
-    public Student getStudent(@RequestParam("admnNo") int admnNo){
-        return studentService.GetStudent(admnNo);
+    public ResponseEntity getStudent(@RequestParam("admnNo") int admnNo){
+        Student response =  studentService.GetStudent(admnNo);
+        return new ResponseEntity<>(response, HttpStatus.FOUND);
     }
 
     //adding the information
     @PostMapping("/add_student")
-    public String addStudent(@RequestBody Student student){
-        return  studentService.AddStudent(student);
+    public ResponseEntity addStudent(@RequestBody Student student){
+        String response = studentService.AddStudent(student);
+        return  new ResponseEntity<>(response,HttpStatus.FOUND);
     }
 
     @DeleteMapping("/delete_student/{id}")
-    public String deleteStudent(@PathVariable("id") int id){
-        return studentService.DeleteStudent(id);
+    public ResponseEntity deleteStudent(@PathVariable("id") int id){
+        String response = studentService.DeleteStudent(id);
+        if(response.equals("Invalid Id")){
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(response, HttpStatus.FOUND);
     }
 
     @PutMapping("/update_student")
-    public String updateStudent(@RequestParam("id") int id ,@RequestParam("age") int age){
-        return studentService.UpdateStudent(id, age);
+    public ResponseEntity updateStudent(@RequestParam("id") int id ,@RequestParam("age") int age){
+        String response =  studentService.UpdateStudent(id, age);
+        if(response.equals("Invalid Id")){
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(response, HttpStatus.FOUND);
     }
 }
